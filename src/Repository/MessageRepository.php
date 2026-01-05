f8c4e5b58c4b690d68c1fba5787b53f24ca1e4bc
anonymous <youremail@example.com>
2026-01-05 15:15:03 +0100

    Ajout de la fonctionnalité pour récupérer les messages entre users

--- src/Repository/MessageRepository.php
+++ src/Repository/MessageRepository.php
@@ -1,9 +1,27 @@
 <?php
-declare(strict_types=1);
 
 namespace App\Repository;
 
-class MessageRepository
-{
+use App\Entity\Message;
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
+class MessageRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Message::class);
+    }
+
+    public function findConversation(User $user1, User $user2): array
+    {
+        return $this->createQueryBuilder('m')
+            ->where('(m.sender = :user1 AND m.recipient = :user2) OR (m.sender = :user2 AND m.recipient = :user1)')
+            ->setParameter('user1', $user1)
+            ->setParameter('user2', $user2)
+            ->orderBy('m.createdAt', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
 }
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -1,9 +1,27 @@
 <?php
-declare(strict_types=1);
 
 namespace App\Repository;
 
-class UserRepository
-{
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
+/**
+ * @extends ServiceEntityRepository<User>
+ */
+class UserRepository extends ServiceEntityRepository
+{
+    // C'est ici que la magie opère :
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+    public function findAllExcept(User $user): array
+    {
+        return $this->createQueryBuilder('u')
+            ->where('u.id != :id')
+            ->setParameter('id', $user->getId())
+            ->getQuery()
+            ->getResult();
+    }
 }
