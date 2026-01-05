8b50d2ef7a2e55e2235033cbc7c6082e353aedda
anonymous <youremail@example.com>
2026-01-05 09:18:20 +0100

    feat: add controller logic

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -2,15 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Annonce;
+use App\Form\AnnonceType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class AnnonceController extends AbstractController
 {
-    #[Route('/create-announce', name: 'app_annonce_create')]
-    public function index(): Response
+    #[Route('/announce/create', name: 'app_announce_create')]
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
+        $annonce = new Annonce();
+        $form = $this->createForm(AnnonceType::class, $annonce);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $annonce->setUtilisateur($this->getUser());
+            $em->persist($annonce);
+            $em->flush();
+            $this->addFlash('success', 'Votre annonce a été publiée avec succès. Elle est désormais visible par les utilisateurs.');
+            return $this->redirectToRoute('app_home');
+        }
         return $this->render('annonce/index.html.twig', [
             'controller_name' => 'AnnonceController',
         ]);
