8b265f6cb5f9fa353afca13ede03fd60b96e8c91
anonymous <youremail@example.com>
2026-01-05 17:13:38 +0100

    feat: ajout des different fichier de test vide

---
+++ tests/Controller/Profile/ProfileCest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Controller\Profile;
+
+use App\Tests\Support\ControllerTester;
+
+final class ProfileCest
+{
+    public function _before(ControllerTester $I): void
+    {
+        // Code here will be executed before each test function.
+    }
+
+    // All `public` methods will be executed as tests.
+    public function tryToTest(ControllerTester $I): void
+    {
+        // Write your test content here.
+    }
+}
