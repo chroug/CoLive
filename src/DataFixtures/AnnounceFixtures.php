de3f228697dbc6968857ee058997cf6de5e5ab52
anonymous <youremail@example.com>
2026-01-06 14:53:39 +0100

    feat: ajout des fixtures de announce

---
+++ src/DataFixtures/AnnounceFixtures.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\AnnounceFactory;
+use App\Factory\UserFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class AnnounceFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        AnnounceFactory::createMany(20, function() {
+            return ['utilisateur' => UserFactory::random()];
+        });
+    }
+
+    public function getDependencies(): array { return [UserFixtures::class]; }
+}
