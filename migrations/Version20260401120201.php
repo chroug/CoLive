c420850886001030f4781231984f5034f81b9141
aguyot32 <guyot.antoine512@gmail.com>
2026-04-02 18:41:02 +0200

    sauvegarde des fichiers de migration avant fusion

--- migrations/Version20260401120201.php
+++ migrations/Version20260401120201.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260118191552 extends AbstractMigration
+final class Version20260401120201 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20260118191552 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE announce ADD is_validated TINYINT DEFAULT 0 NOT NULL');
+        $this->addSql('ALTER TABLE user ADD ville VARCHAR(100) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE announce DROP is_validated');
+        $this->addSql('ALTER TABLE user DROP ville');
     }
 }
